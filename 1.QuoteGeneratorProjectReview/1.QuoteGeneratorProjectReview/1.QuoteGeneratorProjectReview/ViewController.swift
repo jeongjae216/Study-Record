fec44dc6cb674965c1d3feba22166febc28fa767
Jeongjae Lee <wjdwo0413@naver.com>
2022-08-18 01:07:27 +0900

    1.명언생성기 Project Review

---
+++ 1.QuoteGeneratorProjectReview/1.QuoteGeneratorProjectReview/1.QuoteGeneratorProjectReview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1.QuoteGeneratorProjectReview
+//
+//  Created by 정재 on 2022/08/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
